102c89e0c556124aa8c191d6cf43fa08d616153e
Dmitry Morozov <adm713934>
2024-07-23 22:21:46 +0300

    api films, genres, persons added

--- src/api/v1/genres.py
+++ src/api/v1/genres.py
@@ -15,3 +15,26 @@ async def film_details(genre_id: str, genre_service: GenreService = Depends(get_
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='genre not found')
 
     return Genre(name=genre.name)
+
+
+@router.get('/', response_model=list[Genre])
+async def get_genres(
+        page_size: int = 10,
+        page: int = 1,
+        sort: str = 'name',
+        genre_service: GenreService = Depends(get_genre_service)
+) -> list[Genre]:
+    genres = await genre_service.all(page_size, page, sort)
+    return genres
+
+
+@router.get('/search')
+async def search_genres_by_name(
+        page_size: int = 10,
+        page: int = 1,
+        sort: str = 'name',
+        query: str = None,
+        genre_service: GenreService = Depends(get_genre_service)
+):
+    genres = await genre_service.search(page_size, page, sort, query)
+    return genres
--- src/api/v1/persons.py
+++ src/api/v1/persons.py
@@ -15,3 +15,26 @@ async def film_details(person_id: str, person_service: PersonService = Depends(g
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='person not found')
 
     return Person(name=person.name)
+
+
+@router.get('/', response_model=list[Person])
+async def get_persons(
+        page_size: int = 10,
+        page: int = 1,
+        sort: str = 'imdb_rating',
+        person_service: PersonService = Depends(get_person_service)
+) -> list[Person]:
+    persons = await person_service.all(page_size, page, sort)
+    return persons
+
+
+@router.get('/search')
+async def search_persons_by_name(
+        page_size: int = 10,
+        page: int = 1,
+        sort: str = 'imdb_rating',
+        query: str = None,
+        person_service: PersonService = Depends(get_person_service)
+):
+    persons = await person_service.search(page_size, page, sort, query)
+    return persons

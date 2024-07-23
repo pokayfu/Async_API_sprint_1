3cf0020217ee370821357a1ae649ec0b75f5641c
pokayfu <pokayyfu@gmail.com>
2024-07-23 16:54:09 +0300

    feat: основные модели добавлены

--- src/models/film.py
+++ src/models/film.py
@@ -2,7 +2,41 @@
 # Используем pydantic для упрощения работы при перегонке данных из json в объекты
 from pydantic import BaseModel
 
+
 class Film(BaseModel):
     id: str
     title: str
     description: str
+
+
+class Actor(BaseModel):
+    """Класс для описания актёра"""
+    uuid: str
+    name: str
+
+
+class Writer(BaseModel):
+    """Класс для описания сценариста"""
+    uuid: str
+    name: str
+
+
+class Director(BaseModel):
+    """Класс для описания режиссёра"""
+    uuid: str
+    name: str
+
+
+class FullFilm(BaseModel):
+    """Класс для описания всех деталий фильма"""
+    id: str
+    imdb_rating: float | None
+    title: str
+    description: str | None
+    genres: list[str]
+    directors_names: list[str]
+    actors_names: list[str]
+    writers_names: list[str]
+    directors: list[Director]
+    actors: list[Actor]
+    writers: list[Writer]

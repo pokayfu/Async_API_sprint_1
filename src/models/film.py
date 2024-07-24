79a57ff157d0e9723a816ba1181cf1693464f57d
Dmitry Morozov <adm713934>
2024-07-24 08:26:22 +0300

    FilmService updated

--- src/models/film.py
+++ src/models/film.py
@@ -6,3 +6,21 @@ class Film(BaseModel):
     id: str
     title: str
     description: str
+
+
+class Movie(BaseModel):
+    id: str
+    imdb_rating: float | None
+    title: str
+    description: str | None
+    genres: list[str]
+    directors_names: list[str]
+    actors_names: list[str]
+    writers_names: list[str]
+
+    class PersonEntity(BaseModel):
+        id: str
+        name: str
+    directors: list[PersonEntity]
+    actors: list[PersonEntity]
+    writers: list[PersonEntity]
--- src/services/film.py
+++ src/services/film.py
@@ -1,5 +1,8 @@
+import json
 from functools import lru_cache
 from fastapi import Depends
+from orjson import orjson
+
 from src.db.elastic import get_elastic
 from src.db.redis import get_redis
 from typing import Optional
@@ -30,12 +33,62 @@ class FilmService:
 
         return film
 
-    async def _get_film_from_elastic(self, film_id: str) -> Optional[Film]:
+    async def all(self, **kwargs) -> list[Film]:
+        films = await self._films_from_cache(**kwargs)
+        if not films:
+            films = await self._get_films_from_elastic(**kwargs)
+            if not films:
+                return []
+            await self._put_films_to_cache(films, **kwargs)
+        return films
+
+    async def _get_film_from_elastic(self, film_id: str) -> Film | None:
         try:
             doc = await self.elastic.get(index='movies', id=film_id)
         except NotFoundError:
             return None
         return Film(**doc['_source'])
+
+    async def _get_films_from_elastic(self, **kwargs) -> list[Film] | None:
+        page_size = kwargs.get('page_size', 10)
+        page = kwargs.get('page', 1)
+        sort = kwargs.get('sort', '')
+        genre = kwargs.get('genre', None)
+        query = kwargs.get('query', None)
+        body = None
+        if genre:
+            body = {
+                'query': {
+                    'query_string': {
+                        'default_field': 'genre',
+                        'query': genre
+                    }
+                }
+            }
+        if query:
+            body = {
+                'query': {
+                    'match': {
+                        'title': {
+                            'query': query,
+                            'fuzziness': 1,
+                            'operator': 'and'
+                        }
+                    }
+                }
+            }
+        try:
+            docs = await self.elastic.search(index='movies',
+                                             body=body,
+                                             params={
+                                                 'size': page_size,
+                                                 'from': page - 1,
+                                                 'sort': sort,
+                                             })
+        except NotFoundError:
+            return None
+
+        return [await FilmService._make_film_from_es_doc(doc) for doc in docs['hits']['hits']]
 
     async def _film_from_cache(self, film_id: str) -> Optional[Film]:
         # Пытаемся получить данные о фильме из кеша, используя команду get
@@ -48,6 +101,14 @@ class FilmService:
         film = Film.parse_raw(data)
         return film
 
+    async def _films_from_cache(self, **kwargs) -> list[Film] | None:
+        key = await get_key_by_args(**kwargs)
+        data = await self.redis.get(key)
+        if not data:
+            return None
+
+        return [Film.parse_raw(item) for item in orjson.loads(data)]
+
     async def _put_film_to_cache(self, film: Film):
         # Сохраняем данные о фильме, используя команду set
         # Выставляем время жизни кеша — 5 минут
@@ -55,6 +116,20 @@ class FilmService:
         # pydantic позволяет сериализовать модель в json
         await self.redis.set(film.id, film.json(), FILM_CACHE_EXPIRE_IN_SECONDS)
 
+    async def _put_films_to_cache(self, films: list[Film], **kwargs):
+        key = await get_key_by_args(**kwargs)
+        await self.redis.set(key, orjson.dumps([film.json() for film in films]), FILM_CACHE_EXPIRE_IN_SECONDS)
+
+    @staticmethod
+    async def _make_film_from_es_doc(doc: dict) -> Film:
+        return Film(
+            id=doc['_id'],
+            title=doc['_source']['title'],
+            description=doc['_source']['description'],
+        )
+
+    async def get_key_by_args(self, *args, **kwargs) -> str:
+        return f'{args}:{json.dumps({'kwargs': kwargs}, sort_keys=True)}'
 
 @lru_cache()
 def get_film_service(

8975411e15cf8781b79cffdf458ce769725ec3c7
Dmitry Morozov <adm713934>
2024-07-30 19:39:33 +0300

    добавлено кэширование в метод get_films_by_person

--- src/services/film.py
+++ src/services/film.py
@@ -96,7 +96,7 @@ class FilmService:
         return film
 
     async def _films_from_cache(self, **kwargs) -> list[Film] | None:
-        key = await self.get_key_by_args(**kwargs)
+        key = await get_key_by_args(**kwargs)
         data = await self.redis.get(key)
         if not data:
             return None

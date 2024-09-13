8c65b16b566abf5fa6301be8784078ff5fb66ddd
pokayfu <pokayyfu@gmail.com>
2024-09-14 00:45:30 +0300

    feat: lifespan added

--- src/main.py
+++ src/main.py
@@ -7,28 +7,26 @@ from src.db import elastic, redis
 from src.core.config import settings
 from contextlib import asynccontextmanager
 
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    redis.redis = Redis(host=settings.redis_host, port=settings.redis_port)
+    elastic.es = AsyncElasticsearch(hosts=[f'{settings.es_schema}{settings.es_host}:{settings.es_port}'])
+    yield
+    await redis.redis.close()
+    await elastic.es.close()
+
+
 app = FastAPI(
     title=settings.project_name,
     docs_url='/api/openapi',
     openapi_url='/api/openapi.json',
     default_response_class=ORJSONResponse,
+    lifespan=lifespan,
 )
 
-
-@app.on_event('startup')
-async def startup():
-    redis.redis = Redis(host=settings.redis_host, port=settings.redis_port)
-    elastic.es = AsyncElasticsearch(hosts=[f'{settings.es_schema}{settings.es_host}:{settings.es_port}'])
-
-
-@app.on_event('shutdown')
-async def shutdown():
-    await redis.redis.close()
-    await elastic.es.close()
-
-
-# Подключаем роутер к серверу, указав префикс /v1/films
-# Теги указываем для удобства навигации по документации
 app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
 app.include_router(genres.router, prefix='/api/v1/genres', tags=['genres'])
 app.include_router(persons.router, prefix='/api/v1/persons', tags=['persons'])
+

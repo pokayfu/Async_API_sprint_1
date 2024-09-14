c1183ad3ae4d344b5bfae15b6c4ef4487e1f7931
tachen <tatoschka1234@users.noreply.github.com>
2024-09-13 20:59:48 -0700

    docs

--- src/api/v1/films.py
+++ src/api/v1/films.py
@@ -7,15 +7,20 @@ from src.services.film import FilmService, get_film_service
 router = APIRouter()
 
 
-@router.get('/{film_id}', response_model=Film)
+@router.get('/{film_id}', response_model=Film, summary='Retrieve film details by ID')
 async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> Film:
+    """
+    Fetch detailed information about a film, including title, genres, imdb rating, actors, and other information
+    by providing its unique film ID
+    """
     film = await film_service.get_by_id(film_id)
     if not film:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
 
     return Film(**film.model_dump(by_alias=True))
 
-@router.get('/', response_model=list[FilmPreview])
+
+@router.get('/', response_model=list[FilmPreview], summary='Retrieve a list of films with pagination and filters')
 async def get_films(
         page_size: Annotated[int, Query(description='Pagination page size', ge=1)] = 10,
         page: Annotated[int, Query(description='Pagination page number', ge=1)] = 1,
@@ -23,10 +28,15 @@ async def get_films(
         genre: Annotated[str, Query(description='Filter by genre')] = None,
         film_service: FilmService = Depends(get_film_service)
 ) -> list[Film]:
+    """
+    Fetch a paginated list of films with optional sorting by fields like IMDb rating and optional filtering by genre.
+    The response contains a film id, and basic details like title and rating.
+    """
     films = await film_service.all(page_size=page_size, page=page, sort=sort, genre=genre)
     return films
 
-@router.get('/search/', response_model=list[FilmPreview])
+
+@router.get('/search/', response_model=list[FilmPreview], summary='Search films by title with pagination and sorting')
 async def search_films_by_title(
         page_size: Annotated[int, Query(description='Pagination page size', ge=1)] = 10,
         page: Annotated[int, Query(description='Pagination page number', ge=1)] = 1,
@@ -34,5 +44,9 @@ async def search_films_by_title(
         query: Annotated[str, Query(description='Search by film name')] = None,
         film_service: FilmService = Depends(get_film_service)
 ) -> list[FilmPreview]:
+    """
+    Perform a search for films by their title. Supports pagination for managing large result sets and allows sorting
+    by fields such as IMDb rating. The response contains a film id, and basic details like title and imdb rating.
+    """
     films = await film_service.all(page_size=page_size, page=page, sort=sort, query=query)
     return films
--- src/api/v1/genres.py
+++ src/api/v1/genres.py
@@ -9,17 +9,25 @@ from fastapi import APIRouter, Depends, HTTPException, Query
 
 router = APIRouter()
 
-@router.get('/', response_model=list[Genre])
+@router.get('/', response_model=list[Genre], summary='Retrieve a list of film genres with pagination')
 async def get_genres(
         page_size: Annotated[int, Query(description='Pagination page size', ge=1)] = 10,
         page: Annotated[int, Query(description='Pagination page number', ge=1)] = 1,
         genre_service: GenreService = Depends(get_genre_service)
 ) -> list[Genre]:
+    """
+    Fetch a paginated list of available film genres. Supports pagination to navigate through large sets of genres,
+    providing id, name and description fro each genre.
+    """
     films = await genre_service.all(page_size=page_size, page=page)
     return films
 
-@router.get('/{genre_id}', response_model=Genre)
+@router.get('/{genre_id}', response_model=Genre, summary='Retrieve genre details by ID')
 async def genre_details(genre_id: str, genre_service: GenreService = Depends(get_genre_service)) -> Genre:
+    """
+    Fetch information (id, name and description) about a specific genre by providing its unique genre ID. If the genre is not found,
+    a 404 error will be returned.
+    """
     genre = await genre_service.get_by_id(genre_id)
     if not genre:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='genre not found')
